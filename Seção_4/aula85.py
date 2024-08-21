35ed49d078c692aac17a7f1352bb31e1b637e8ad
Henry Mori <henry.mori@karpat.com.br>
2024-08-21 16:33:18 -0300

    aula85.py

---
+++ Seção_4/aula85.py
@@ -0,0 +1,13 @@
+"""
+Introdução as funções (def) em python
+Funções são trechos de códigos usados para
+replicar determinada ação ao longo do seu codigo
+Elas podem receber valores para parametros (argumentos)
+e retornar um valor especifico.
+Por padrão, funções Python retornam None (nada).
+"""
+
+def Print():
+    print('Variavel')
+
+Print()
